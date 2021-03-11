057b4b82ba02eda7a98a4769afacd3ce88e97af5
Ryan Kinnard <ryan.cb.kinnard@gmail.com>
2021-03-10 20:09:26 -0500

    add compose tweet functionality

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,16 +3,23 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
 
     public String body;
     public String createdAt;
     public User user;
     public long id;
+
+    // empty constructor needed by the Parceler library
+    public Tweet() {
+
+    }
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
